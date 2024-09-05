4da70d324d558688038f39cea6847044d921e4ca
Ranjith Thangaraju <ranjithshuttler10@gmail.com>
2024-09-05 19:26:38 +0530

    landing page - responsive

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,36 +1,53 @@
-import HeroImage from '../../assets/hero-image.jpg';
+import HeroImage from "../../assets/hero-image.jpg";
+import "./Home.css";
 
-export function Home(){
-    return (
-        <>
-            <div className='hidden md:flex h-[80%] items-center justify-center'>
-                <div className='flex flex-row mx-[10%] justify-center'>
-                    <div className='flex flex-col justify-around'>
-                        <div className='text-3xl text-primary font-title'>CREATE MEMORIES</div>
-                        <div className='text-lg font-light text-secondary font-display leading-[250%]'>
-                            Customized photobooks and calendars are fantastic ways to preserve and celebrate memories. 
-                            They allow you to curate your favorite moments, organizing them into a meaningful narrative that can be revisited anytime. 
-                            Each page becomes a special reminder of significant times, making them deeply personal gifts. 
-                        </div>
-                        <div className='font-display ml-[7%]'>
-                            <button className='text-md text-primary bg-accent rounded-sm font-medium py-[2%] px-[8%]'>PRINT MEMORIES</button>
-                        </div>
-                    </div>
-                    <div className='flex flex-col items-center py-[2%]'>
-                        <img className='rounded-lg w-4/6 h-auto' src={HeroImage} alt='Hero'/>
-                    </div>
-                </div>
+export function Home() {
+  return (
+    <>
+      <div className="hidden md:flex h-4/5 items-center">
+        <div className="flex flex-row lg:px-[8%] justify-around w-full items-center">
+          <div className="flex flex-col md:gap-3 xl:gap-6 w-[45%]">
+            <div className="text-3xl xl:text-4xl text-primary font-title">
+              CREATE MEMORIES
             </div>
-            <div className='md:hidden flex flex-col items-center overscroll-auto'>
-                <img className='my-8 rounded-lg w-4/6 h-auto' src={HeroImage} alt='Hero'/>
-                <div className='mt-2 text-3xl text-primary font-title'>CREATE MEMORIES</div>
-                <div className='mt-5 text-center text-lg font-light text-secondary font-display leading-[230%]'>
-                    Customized photobooks and calendars are fantastic ways to preserve and celebrate memories. 
-                    They allow you to curate your favorite moments, organizing them into a meaningful narrative that can be revisited anytime. 
-                    Each page becomes a special reminder of significant times, making them deeply personal gifts. 
-                </div>
-                <button className='mt-5 font-display text-lg text-primary bg-accent rounded-sm font-medium py-3 px-10 w-5/6'>PRINT MEMORIES</button>                
+            <div className="md:max-h-[30vh] lg:max-h-[35vh] xl:max-h-[45vh] md:text-lg xl:text-xl font-light text-secondary font-display md:leading-[250%] xl:leading-[300%] hero-content text-ellipsis overflow-hidden">
+              Customized photobooks and calendars are fantastic ways to preserve
+              and celebrate memories. They allow you to curate your favorite
+              moments, organizing them into a meaningful narrative that can be
+              revisited anytime. Each page becomes a special reminder of
+              significant times, making them deeply personal gifts.
             </div>
-        </>
-    );
+            <div className="font-display text-lg text-primary ms-[10%]">
+              <button className="bg-accent rounded-sm font-medium py-[2.5%] md:px-[15%] xl:px-[10%]">
+                PRINT MEMORIES
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-center w-[45%]">
+            <img className="rounded-lg hero-image" src={HeroImage} alt="Hero" />
+          </div>
+        </div>
+      </div>
+      <div className="md:hidden flex flex-col items-center overscroll-auto">
+        <img
+          className="my-8 rounded-lg w-[70vw] sm:w-[60vw] h-auto"
+          src={HeroImage}
+          alt="Hero"
+        />
+        <div className="mt-2 text-3xl text-primary font-title">
+          CREATE MEMORIES
+        </div>
+        <div className="mt-5 px-2 text-center text-lg font-light text-secondary font-display leading-[230%]">
+          Customized photobooks and calendars are fantastic ways to preserve and
+          celebrate memories. They allow you to curate your favorite moments,
+          organizing them into a meaningful narrative that can be revisited
+          anytime. Each page becomes a special reminder of significant times,
+          making them deeply personal gifts.
+        </div>
+        <button className="mt-5 font-display text-lg text-primary bg-accent rounded-sm font-medium py-3 px-10 w-5/6">
+          PRINT MEMORIES
+        </button>
+      </div>
+    </>
+  );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import logo from '../../assets/sasitha-logo.svg';
+import {XMarkIcon, Bars3Icon} from "@heroicons/react/16/solid"
+
 
 const navigation = [
   { name: 'Home', href: '#', current: true },
@@ -9,6 +12,14 @@ const navigation = [
 ]
 
 export default function Navbar() {
+
+    const [isOpen, setOpen] = useState(false);
+
+    function navMenu(){
+        console.log(isOpen);
+        setOpen(!isOpen);
+    }
+
   return (
     <>
         <div className='hidden md:block'>
@@ -29,13 +40,19 @@ export default function Navbar() {
         <div className='md:hidden overscroll-auto'>
             <div className='h-10'/>
             <div className="font-display flex justify-between mb-5 mx-4">
-                <img className='w-4/12 h-auto' src={logo} alt='brand'/>
-                <button type="button">
-                    <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
+                <img className='h-[3vh] w-auto' src={logo} alt='brand'/>
+                <button type="button" onClick={navMenu}>
+                   {!isOpen && <Bars3Icon className="size-6 text-primary"/>}
+                   {isOpen && <XMarkIcon className="size-6 text-primary"/>}
                 </button>
             </div>
+            {isOpen && <div className='grid my-10 grid-flow-row gap-9'>
+                {navigation.map(item => {
+                    return (
+                        <div className={`${item.current? 'font-semibold' : 'font-light'} text-lg text-center`}>{item.name}</div>
+                    )
+                })}
+            </div>}
         </div>
     </>
   )
